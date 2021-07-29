5f4c0dbfd572b1e0326fe45eaeb75f08a8f3818c
Daniel O <danielogunlana23@gmail.com>
2021-07-29 18:36:31 -0400

    updated queries and mutations client side

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,61 @@
+import { gql } from "@apollo/client";
+
+export const ADD_USER = gql`
+	mutation addUser($username: String!, $email: String!, $password: String!) {
+		addUser(username: $username, email: $email, password: $password) {
+			token
+			user {
+				_id
+				username
+			}
+		}
+	}
+`;
+
+export const LOGIN_USER = gql`
+	mutation login($email: String!, $password: String!) {
+		login(email: $email, password: $password) {
+			token
+			user {
+				_id
+				username
+				email
+				password
+			}
+		}
+	}
+`;
+//bookData: BookSearch!
+export const SAVE_BOOK = gql`
+	mutation saveBook($bookData: BookSearch!) {
+		saveBook(bookData: $bookData) {
+			_id
+			username
+			email
+			savedBooks {
+				bookId
+				authors
+				image
+				description
+				link
+			}
+		}
+	}
+`;
+
+export const DELETE_BOOK = gql`
+	mutation deleteBook($bookId: String!) {
+		deleteBook(bookId: $bookId) {
+			id
+			username
+			savedBooks {
+				_id
+				title
+				authors
+				description
+				image
+				link
+			}
+		}
+	}
+`;
