8152092e393fea4fb8221f949dfb0da96b5d280b
Daniel O <danielogunlana23@gmail.com>
2021-07-29 19:07:43 -0400

    installed apollo client and other depedencies and also fixed server middleware

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Form, Button, Alert } from "react-bootstrap";
 
-import { createUser } from "../utils/API";
+import { ADD_USER } from "../utils/mutations";
 import Auth from "../utils/auth";
 import { useMutation } from "@apollo/client";
 
@@ -16,6 +16,8 @@ const SignupForm = () => {
 	const [validated] = useState(false);
 	// set state for alert
 	const [showAlert, setShowAlert] = useState(false);
+
+	const [addUser] = useMutation(ADD_USER);
 
 	const handleInputChange = (event) => {
 		const { name, value } = event.target;
